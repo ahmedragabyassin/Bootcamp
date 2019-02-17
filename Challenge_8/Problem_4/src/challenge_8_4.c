582fe500763752688001c6e50afe5fe769a77696
ahmedragabyassin <47712241+ahmedragabyassin@users.noreply.github.com>
2019-02-17 13:18:17 +0200

    Problem 4 in challenge 8 added

---
+++ Challenge_8/Problem_4/src/challenge_8_4.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : challenge_8_4.c
+ Author      : Yassin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * ==========================================================================
+ * Team Name: Xcoderz
+ * Team members: Ahmed Yassin, Ahmed Abdrehem
+ *  =========================================================================
+ */
+int main(void) {
+	BinarySearchOccurances_test();
+	return EXIT_SUCCESS;
+}
---
+++ Challenge_8/Problem_4/src/problem_four.c
@@ -0,0 +1,76 @@
+/*********************************************************************************
+ *
+ *  problem_four.c
+ *  Author: Ahmed Nofal, Avelabs Embedded software R&D crew
+ *
+ ********************************************************************************/
+
+/****************************[Problem Description]*********************************
+ *
+ * Count occurances of a number in a sorted array
+ * Sample Input: arr= [2,2, 4, 5, 5, 5, 5, 6, 6]
+ * 				 target = 5
+ * Sample Output: The number 5 occured 4 times in the array
+ *
+ ********************************************************************************/
+#include <stdio.h>
+#include "problem_four.h"
+
+/*********************************************************************************
+ *
+ * Function: BinarySearchOccurances
+ *
+ * @param: arr[], Array that the function should find the number in.
+ * @param: arr_size, Size of the array
+ * @param: target, The elment to be searched for the last occurance for
+ * @param: searchFirst, Indicates whether you are using this function to get the
+ * first occurance of the target or last (1 for first and 0 for last)
+ * @return: int, the index of the last occurance of the element
+ *
+ * Description: function to get the number of occurances of a target element in a
+ * given sorted array.
+ *
+ *
+ ********************************************************************************/
+int BinarySearchOccurances(int arr[], int arr_size, int target, int searchFirst){
+	/* Your code goes here */
+	int i;
+	int occurrance = 0;
+	if(searchFirst == 1)
+	{
+		for(i=0; i<arr_size;i++)
+		{
+			if(target == arr[i])
+				return i;
+		}
+	}
+	else if(searchFirst == 0)
+	{
+		for(i=0; i<arr_size; i++)
+		{
+			if(target == arr[i])
+				occurrance = i;
+		}
+		return occurrance;
+	}
+
+	return -1;
+}
+
+/* Count occurrences of a number in a sorted array with duplicates*/
+void BinarySearchOccurances_test(void)
+{
+	int A[] = {2, 5, 5, 5, 6, 6, 8, 9, 9, 9};
+	int target = 5;
+	int n = sizeof(A)/sizeof(A[0]);
+	int first = BinarySearchOccurances(A, n, target, 1);  // 1 for first occurrence
+	int last = BinarySearchOccurances(A, n, target, 0);   // 0 for last occurrence
+
+	int count = last - first + 1;
+
+	if (first != -1)
+		printf("Element %d occurs %d times.", target, count);
+	else
+		printf("Element not found in the array.");
+}
+
