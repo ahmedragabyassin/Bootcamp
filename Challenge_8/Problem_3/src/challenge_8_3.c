1897be593d596bc5f27b236230e8c79287102a89
ahmedragabyassin <47712241+ahmedragabyassin@users.noreply.github.com>
2019-02-17 13:05:17 +0200

    Problem 3 in challenge 8 added

---
+++ Challenge_8/Problem_3/src/challenge_8_3.c
@@ -0,0 +1,25 @@
+/*
+ ============================================================================
+ Name        : challenge_8_3.c
+ Author      : Yassin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * ==========================================================================
+ * Team Name: Xcoderz
+ * Team members: Ahmed Yassin, Ahmed Abdrehem
+ *  =========================================================================
+ */
+
+
+int main(void) {
+	findLastOccurrence_test();
+	return EXIT_SUCCESS;
+}
